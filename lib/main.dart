986183c387285e8c05865e1132c6e08b8e8d8e67
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-14 23:23:02 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContextcontext) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Container example"),
+        ),
+        body: Container(
+          child: const Text(
+            'Hello i am inside container!',
+          ),
+        ),
+      ),
+    );
+  }
+}
