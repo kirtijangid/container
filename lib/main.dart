6dcc617a25fe28664f24815a9709215f3b8b0875
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-14 23:37:45 +0530

    giving bg to the just the text

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
           title: const Text("Container example"),
         ),
         body: Container(
+          color: Colors.yellowAccent,
           child: const Text(
             'Hello i am inside container!',
             style: TextStyle(
